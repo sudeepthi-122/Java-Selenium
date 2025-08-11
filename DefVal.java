025a6a82ac5d466150c4af7992c1a6f41c5aee4e
sudeepthi-122 <113970860+sudeepthi-122@users.noreply.github.com>
2025-08-11 10:34:51 +0530

    Add files via upload

---
+++ DefVal.java
@@ -0,0 +1,24 @@
+class DefaultValue{
+	public static void main(String...args){
+		//A variable is declared
+		byte b;
+		short s;
+		int x;
+		long k;
+		float f;
+		double d;
+		boolean val;
+		char c;
+		
+		System.out.println("Default byte variable value: "+b);
+		System.out.println("Default short variable value: "+s);
+		System.out.println("Default int variable value: "+x);
+		System.out.println("Default long variable value: "+k);
+		System.out.println("Default float variable value: "+f);
+		System.out.println("Default double variable value: "+d);
+		System.out.println("Default boolean variable value: "+val);
+		System.out.println("Default char variable value: "+c);
+		
+		
+	}
+}	
